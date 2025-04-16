ef04d7a0afa802acc898c30e3548118ac7a3ba6d
charandoli <kali.dkc@gmail.com>
2025-04-16 15:48:29 -0400

    Initial commit

---
+++ src/main/java/com/example/stock_porfolio/StockPorfolioApplication.java
@@ -0,0 +1,13 @@
+package com.example.stock_porfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPorfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPorfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stock_porfolio/StockPorfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stock_porfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPorfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
