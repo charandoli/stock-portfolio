b2d7d97d57cb8bf00f7e535560995bb56315ad51
charandoli <kali.dkc@gmail.com>
2025-05-15 12:38:20 -0400

    Update to add support for OpenAPI

--- src/main/java/com/example/stock_portfolio/StockPortfolioApplication.java
+++ src/main/java/com/example/stock_portfolio/StockPortfolioApplication.java
@@ -1,9 +1,12 @@
 package com.example.stock_portfolio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "StockPortfolio API", version = "1.0", description = "API documentation for Stock portfolio Spring Boot App"))
 public class StockPortfolioApplication {
 
 	public static void main(String[] args) {
